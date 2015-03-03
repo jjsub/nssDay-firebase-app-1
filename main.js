0f404d04e492873cfa500aa62b76548dc80ab5dd
jjsub <jsamesub@gmail.com>
2015-03-02 19:09:36 -0600

    main.js modular

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-var firebase = require("firebase");
+/*var firebase = require("firebase");
 
 var myFirebaseRef = new Firebase("https://fiery-fire-2497.firebaseio.com/");
+*/
+
+
+var jjCurrentMessage    = document.getElementById('jjCurrentMessage'), 
+          txtNewMassage       = document.getElementById('txtNewMassage'),
+          btUpdateMessage     = document.getElementById('btUpdateMessage'),
+          rootRef = new Firebase('https://realtime-messege.firebaseio.com/'),
+          currentMessageRef = rootRef.child('currentMessage');
+
+          btUpdateMessage.addEventListener('click', function(){
+
+            currentMessageRef.set(txtNewMassage.value);
+            //this will clear the input box after summit
+            txtNewMassage.value = '';
+
+          });
+
+          currentMessageRef.on('value', function(snapshot){
+              jjCurrentMessage.innerText = snapshot.val();
+          });
